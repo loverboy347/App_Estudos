a1bd099e3da8f5facb998c6f7b18f542b0ff1d71
loverboy347 <rigottosilvav@gmail.com>
2026-09-20 18:35:24 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
-const CACHE='bio-estudos-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='bio-estudos-v2';
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(['./', './index.html', './manifest.webmanifest', './sw.js']))
+  );
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request).then(network => {
+      const copy = network.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return network;
+    }).catch(() => caches.match('./index.html'))))
+  );
+});
